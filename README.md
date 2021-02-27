79b868298e9f6f4908e895ba224a9313ea5aedd8
Tri Budiyono <tribudiyono39@gmail.com>
2021-02-27 16:19:22 +0700

    add echo

--- README.md
+++ README.md
@@ -3,3 +3,23 @@ fetch dependencies from go.mod :
 
 run server :
 - go run cmd/simple-service/main.go
+
+curl create user :
+curl --location --request POST 'http://127.0.0.1:1323/users' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "name": "tri"
+}'
+
+curl get user :
+curl --location --request GET 'http://127.0.0.1:1323/users/1'
+
+curl update user :
+curl --location --request PUT 'http://127.0.0.1:1323/users/1' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "name": "tri budiyono"
+}'
+
+curl delete user :
+curl --location --request DELETE 'http://127.0.0.1:1323/users/1'
--- cmd/simple-service/main.go
+++ cmd/simple-service/main.go
@@ -1,18 +1,70 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
+type (
+	user struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
+)
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+var (
+	users = map[int]*user{}
+	seq   = 1
+)
+
+func createUser(c echo.Context) error {
+	u := &user{
+		ID: seq,
 	}
 
-	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listring for request at http://127.0.0.1:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+func updateUser(c echo.Context) error {
+	u := new(user)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+
+	id, _ := strconv.Atoi(c.Param("id"))
+	users[id].Name = u.Name
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func deleteUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(users, id)
+	return c.NoContent(http.StatusNoContent)
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
